1fcb822416cbd36d230a25455f5b23fffbc0cd0d
GiornoBananaa <105521406+GiornoBananaa@users.noreply.github.com>
2023-09-21 19:13:23 +0300

    Update CountryHouse.cs

--- Kirillov_KT1/Kirillov_KT1/CountryHouse.cs
+++ Kirillov_KT1/Kirillov_KT1/CountryHouse.cs
@@ -4,23 +4,6 @@ public class CountryHouse  : Immovable
 {
     public CountryHouse(float worth, float square) : base(worth, square)
     {
-        switch (square)
-        {
-            case < 100:
-                _taxRate = 0.002f;
-                break;
-            case >= 100 and < 300:
-                _taxRate = 0.0029f;
-                break;
-            case >= 300:
-                _taxRate = 0.004f;
-                break;
-        }
-    }
-    
-    public override float СalculateTax()
-    {
-        return Worth * _taxRate;
     }
     
     public override string ToString()
