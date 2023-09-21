6e0ec3d238bde11f81eb613f1614fb50328fde3a
GiornoBananaa <105521406+GiornoBananaa@users.noreply.github.com>
2023-09-21 18:58:41 +0300

    Update Vector2.cs

--- Kirillov_Generics/Kirillov_Generics_4-5/Vector2.cs
+++ Kirillov_Generics/Kirillov_Generics_4-5/Vector2.cs
@@ -5,9 +5,14 @@ public struct Vector2
     public float X;
     public float Y;
 
-    public Vector2(float y,float x)
+    public Vector2(float x,float y)
     {
         X = x;
         Y = y;
+    }
+    
+    public override string ToString()
+    {
+        return $"({X.ToString(CultureInfo.InvariantCulture)},{Y.ToString(CultureInfo.InvariantCulture)})";
     }
 }
