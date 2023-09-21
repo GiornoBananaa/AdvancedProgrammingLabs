bc353c1c85f0b30a750d176843f2dbcbdbbd28e7
GiornoBananaa <105521406+GiornoBananaa@users.noreply.github.com>
2023-09-21 18:57:55 +0300

    Update Rectangle.cs

--- Kirillov_Generics/Kirillov_Generics_4-5/Rectangle.cs
+++ Kirillov_Generics/Kirillov_Generics_4-5/Rectangle.cs
@@ -4,11 +4,28 @@ public class Rectangle<TW,TH>: Figure
 {
     public TW Width { get; protected set; }
     public TH Height { get; protected set; }
-   
+
+    public Vector2 MinPoint => new(Center.X - ConvertToFloat(Width) / 2, 
+        Center.Y + ConvertToFloat(Height) / 2 );
+    public Vector2 MaxPoint => new(Center.X + ConvertToFloat(Width) / 2, 
+        Center.Y - ConvertToFloat(Height) / 2 );
    
     public Rectangle(Vector2 center, TW width,TH height) : base(center)
     {
         Width = width;
         Height = height;
+    }
+
+    private float ConvertToFloat(object value)
+    {
+        float result = value switch
+        {
+            float f => f,
+            int i => i,
+            double => (float)Convert.ToDouble(value),
+            string s => float.Parse(s)
+        };
+
+        return result;
     }
 }
