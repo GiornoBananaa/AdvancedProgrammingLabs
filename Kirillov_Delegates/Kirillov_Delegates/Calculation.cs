d6df46d5a10f4d42e4c199c727e6e63aefd88d86
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-10-11 17:27:58 +0300

    Added main

---
+++ Kirillov_Delegates/Kirillov_Delegates/Calculation.cs
@@ -0,0 +1,97 @@
+﻿namespace Kirillov_Delegates;
+
+static class Calculation
+{
+    public delegate float FactorialDelegate(object obj);
+    public delegate int NumSumDelegate (object obj);
+    public delegate int NumMultDelegate (object obj);
+    public delegate void PrintSumDelegate (object obj);
+    public delegate void PrintMultDelegate (object obj);
+    
+    public static float Factorial(object t)
+    {
+        float f = ConvertFloat(t);
+        
+        if(f == 0)
+            return 1;
+        else
+            return f * Factorial(f-1);
+    }
+
+    public static int NumSum(object t)
+    {
+        int f = (int)ConvertFloat(t);
+        
+        int sum = 0;
+        while (f > 0)
+        {
+            sum += f % 10;
+            f = f /10 ;
+        }
+
+        return sum;
+    }
+    
+    public static int NumMult(object t)
+    {
+        int f = (int)ConvertFloat(t);
+        
+        int mult = 1;
+        while (f > 0)
+        {
+            mult *= f % 10;
+            f = f /10 ;
+ 
+        }
+
+        return mult;
+    }
+
+    public static void PrintSum(object f)
+    {
+        Console.WriteLine(NumSum(f));
+    }
+    
+    public static void PrintMult(object f)
+    {
+        Console.WriteLine(NumMult(f));
+    }
+
+    public static bool RemoveOdd(int f, out int deletedCount,out int result)
+    {
+        string fstr = f.ToString();
+        deletedCount = fstr.Length;
+        fstr = fstr.Replace("0", string.Empty)
+            .Replace("2", string.Empty)
+            .Replace("4", string.Empty)
+            .Replace("6", string.Empty)
+            .Replace("8", string.Empty);
+
+        result = fstr.Length > 0 ? int.Parse(fstr) : 0;
+        deletedCount -= fstr.Length;
+        return deletedCount > 0;
+    }
+
+    private static float ConvertFloat(object t)
+    {
+        float f = 0;
+        if (t is float)
+        {
+            f = (float)t;
+        }
+        else if (t is int)
+        {
+            f = (int)t;
+        }
+        else if (t is double)
+        {
+            f = (float)Convert.ToDouble(t);
+        }
+        else if (t is string)
+        {
+            f = float.Parse((string)t);
+        }
+
+        return f;
+    }
+}
--- Kirillov_Delegates/Kirillov_Delegates/Program.cs
+++ Kirillov_Delegates/Kirillov_Delegates/Program.cs
@@ -4,68 +4,20 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        
-    }
-}
+        Calculation.FactorialDelegate factorialDelegate = Calculation.Factorial;
+        Calculation.NumSumDelegate numSumDelegate = Calculation.NumSum;
+        Calculation.NumMultDelegate numMultDelegate = Calculation.NumMult;
+        Calculation.PrintSumDelegate printSumDelegate = Calculation.PrintSum;
+        Calculation.PrintMultDelegate printMultDelegate = Calculation.PrintMult;
 
-static class Calculation
-{
-    public static int Factorial(int f)
-    {
-        if(f == 0)
-            return 1;
-        else
-            return f * Factorial(f-1);
-    }
+        Console.WriteLine(factorialDelegate.Invoke(5));
+        Console.WriteLine(numSumDelegate.Invoke(1211));
+        Console.WriteLine(numMultDelegate.Invoke(123));
+        printSumDelegate.Invoke(412);
+        printMultDelegate.Invoke(412);
 
-    public static int NumSum(int f)
-    {
-        int sum = 0;
-        while (f > 0)
-        {
-            sum += f % 10;
-            f = f /10 ;
- 
-        }
-
-        return sum;
-    }
-    
-    public static int NumMult(int f)
-    {
-        int sum = 0;
-        while (f > 0)
-        {
-            sum *= f % 10;
-            f = f /10 ;
- 
-        }
-
-        return sum;
-    }
-
-    public static void PrintSum(int f)
-    {
-        NumSum(f);
-    }
-    
-    public static void PrintMult(int f)
-    {
-        NumMult(f);
-    }
-
-    public static bool RemoveOdd(int f, out int deletedCount,out int result)
-    {
-        string fstr = f.ToString();
-        deletedCount = fstr.Length;
-        fstr = fstr.Replace("0", string.Empty)
-            .Replace("2", string.Empty)
-            .Replace("4", string.Empty)
-            .Replace("6", string.Empty)
-            .Replace("8", string.Empty);
-
-        result = fstr.Length > 0 ? int.Parse(fstr) : 0;
-        deletedCount -= fstr.Length;
-        return deletedCount > 0;
+        Calculation.PrintSumDelegate printDelegate = Calculation.PrintSum;
+        printDelegate += Calculation.PrintMult;
+        printDelegate.Invoke(572);
     }
 }
