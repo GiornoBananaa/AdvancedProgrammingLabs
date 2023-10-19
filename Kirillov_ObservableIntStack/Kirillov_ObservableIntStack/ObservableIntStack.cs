76cff04673aafb1263debd3ca1c5e447ccb95623
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-10-19 23:09:51 +0300

    Finished Task

---
+++ Kirillov_ObservableIntStack/Kirillov_ObservableIntStack/ObservableIntStack.cs
@@ -0,0 +1,64 @@
+﻿public class ObservableIntStack
+{
+    public event Action<int> OnItemAdded;
+    public event Action<int> OnItemRemoved;
+    private Stack<int> _stack;
+   
+    public ObservableIntStack(IEnumerable<int> numbers)
+    {
+        _stack = new Stack<int>();
+        foreach (var i in numbers)
+        {
+            _stack.Push(i);
+        }
+    }
+   
+    public int Count => _stack.Count;
+   
+    public void SubscribeToAdding(Action<int> subscriber)
+    {
+        if (subscriber == null)
+            throw new ArgumentNullException();
+
+        OnItemAdded += subscriber;
+    }
+   
+    public void SubscribeToRemoving(Action<int> subscriber)
+    {
+        if (subscriber == null)
+            throw new ArgumentNullException();
+
+        OnItemRemoved += subscriber;
+    }
+   
+    public void UnsubscribeFromAdding(Action<int> subscriber)
+    {
+        if (subscriber == null)
+            throw new ArgumentNullException();
+
+        OnItemAdded -= subscriber;
+    }
+   
+    public void UnsubscribeFromRemoving(Action<int> subscriber)
+    {
+        if (subscriber == null)
+            throw new ArgumentNullException();
+
+        OnItemRemoved -= subscriber;
+    }
+
+    public void Push(int item)
+    {
+        _stack.Push(item);
+        OnItemAdded(item);
+    }
+    public int Pop()
+    {
+        int item = _stack.Pop();
+        OnItemRemoved(item);
+        return item;
+    }
+
+    public int Peek() => _stack.Peek();
+
+}
---
+++ Kirillov_ObservableIntStack/Kirillov_ObservableIntStack/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    private static event Action<int> addSubscriber;
+    private static event Action<int> removeSubscriber;
+   
+    static void Main(string[] args)
+    {
+        ObservableIntStack stack = new ObservableIntStack(new int[]{1,2,3,4,5,6});
+
+        addSubscriber += NotificationAdded;
+        addSubscriber += NotificationNumberParity;
+        removeSubscriber += NotificationRemoved;
+
+        stack.SubscribeToAdding(addSubscriber);
+        stack.SubscribeToRemoving(removeSubscriber);
+        stack.Push(7);
+        stack.Push(8);
+
+        stack.Pop();
+        stack.Pop();
+    }
+   
+    private static void NotificationAdded(int item)
+    {
+        Console.WriteLine($"{item} - successfully added to stack!");
+    }
+   
+    private static void NotificationNumberParity(int item)
+    {
+        Console.WriteLine($"New item {item} is {(item % 2 == 0 ? "even" : "odd")} number");
+    }
+   
+    private static void NotificationRemoved(int item)
+    {
+        Console.WriteLine($"{item} - successfully removed from stack!");
+    }
+}
