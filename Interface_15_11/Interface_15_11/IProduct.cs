59dbc900200f9b2c4f69326b98644978023109d9
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-11-15 20:03:22 +0300

    Finished task 1 and 2

---
+++ Interface_15_11/Interface_15_11/IProduct.cs
@@ -0,0 +1,20 @@
+﻿namespace Interface_15_11;
+
+public interface IProduct
+{
+    public string Name { get; }
+    public float Cost { get; }
+    public string Description { get; }
+    
+    public float GetDeliveryCost(int distance);
+    public string GetProductInfo()
+    {
+        string information = $"------Product info-------\n" +
+                             $"Name: {Name}\n" +
+                             $"Cost: {Cost}\n" +
+                             $"      Description\n" +
+                             $"{Description}\n" +
+                             $"-------------------------";
+        return information;
+    }
+}
