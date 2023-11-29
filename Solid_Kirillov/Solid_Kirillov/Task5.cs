3becad949db0f5dcb4bd4fb9cbf19578fabcd8bd
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-11-29 18:26:09 +0300

    small task5 changes

--- Solid_Kirillov/Solid_Kirillov/Task5.cs
+++ Solid_Kirillov/Solid_Kirillov/Task5.cs
@@ -2,13 +2,46 @@
 
 public class SmartHouseController
 {
-    public void ChangeVolume(ISoundMaker soundMaker, float volume)
+    public void TurnOffAllSounds(float volume)
     {
-        soundMaker.ChangeVolume(volume);
+        
     }
 }
 
 public interface ISoundMaker
 {
+    
     public void ChangeVolume(float volume);
+    public void MuteSound(bool muted);
+}
+
+public class YandexStation: ISoundMaker
+{
+    private float _volume;
+    private bool _isMuted;
+    
+    public void ChangeVolume(float volume)
+    {
+        _volume = volume;
+    }
+
+    public void MuteSound(bool muted)
+    {
+        _isMuted = muted;
+    }
+}
+
+public class TV: ISoundMaker
+{
+    private float _volume;
+    private bool _isMuted;
+    
+    public void ChangeVolume(float volume)
+    {
+        _volume = volume;
+    }
+    public void MuteSound(bool muted)
+    {
+        _isMuted = muted;
+    }
 }
