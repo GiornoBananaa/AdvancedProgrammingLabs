094089c526857920ac7858aadda49753fab7028c
unknown <student@infra.ithub.ru>
2023-12-14 16:35:25 +0300

    Added IKillable and ICombatCapable interfaces

--- KT_Final/KT_Final/Program.cs
+++ KT_Final/KT_Final/Program.cs
@@ -9,11 +9,11 @@ public class World
         public Vector2
     }*/
     
-    public GameObject?[][] WorldMap;
+    public GameObject?[,] WorldMap;
     
     public World(int size)
     {
-        
+        WorldMap = new GameObject?[size,size];
     }
     
     public void AddEnemy()
@@ -49,20 +49,65 @@ public struct Vector2
     }
 }
 
-public class GameObject
+public abstract class GameObject
 {
     private World _world;
     private Vector2 _position;
+    
+    public GameObject(World world, Vector2 position)
+    {
+        _world = world;
+        _position = position;
+    }
     
     public Vector2 Position
     {
         get => _position;
         private set
         {
-            if(_world.WorldMap[_position.X][_position.Y] != null) 
+            if(_world.WorldMap[value.X,value.Y] != null) 
                 return;
-            _world.WorldMap[_position.X][_position.Y] = this;
+            _world.WorldMap[value.X,value.Y] = this;
+            _world.WorldMap[_position.X,_position.Y] = null;
             _position = value;
         }
     }
+}
+
+public interface IKillable
+{
+    public int HpPercentage => Hp * 100 / MaxHp;
+    protected int MaxHp { get; set; }
+    protected int Hp { get; set; }
+    
+    public void TakeDamage(int damage)
+    {
+        if (damage <= 0) return;
+        
+        Hp -= damage;
+        if (Hp <= 0)
+        {
+            Hp = 0;
+            Die();
+        }
+    }
+    
+    public void Heal(int hp)
+    {
+        if (hp <= 0) return;
+        
+        Hp += hp;
+        if (Hp > MaxHp)
+            Hp = MaxHp;
+    }
+    
+    protected void Die();
+}
+
+public interface ICombatCapable
+{
+    protected int AttackForce { get; set; }
+    protected int AttackRange { get; set; }
+
+    public void Attack(IKillable target);
 }
