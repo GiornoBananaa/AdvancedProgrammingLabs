26163fafe3121f024ff28d3d23ed1966bbf331ad
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2024-03-04 20:43:07 +0300

    Finsihed tasks

--- Kirillov_Generic_Covariance/Program.cs
+++ Kirillov_Generic_Covariance/Program.cs
@@ -1,3 +1,135 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics.CodeAnalysis;
 
-Console.WriteLine("Hello, World!");
+namespace Kirillov_Generic_Covariance;
+
+public class IncorrectOptionalAccessException : InvalidOperationException
+{
+}
+
+public interface IOptional<out T, in K> where K:T
+{
+    public T Value { get; }
+    public bool Empty { get; }
+    void SetValue(K? value);
+    T GetValueOrDefault();
+}
+
+public class Optional<T,K>: IOptional<T,K> where K:T
+{
+    private T _value;
+    public T Value
+    {
+        get
+        {
+            if (Empty)
+                throw new IncorrectOptionalAccessException();
+            return _value;
+        }
+        set => _value = value;
+    }
+
+    public bool Empty { get; protected set; }
+
+    public Optional()
+    {
+        Empty = true;
+    }
+    
+    public Optional(T value)
+    {
+        _value=value;
+    }
+    
+    public virtual void SetValue(K? value)
+    {
+        if (value == null)
+            Empty = true;
+        else
+        {
+            Empty = false;
+            Value = (T)value;
+        }
+    }
+
+    public virtual T GetValueOrDefault()
+    {
+        if (Empty)
+            return default;
+        return Value;
+    }
+
+    public override string ToString()
+    {
+        if (Empty)
+            return "empty";
+        
+        return Empty? "empty":_value.ToString();
+    }
+}
+
+public class ExtendedOptional<T, K>: Optional<T, K> where K:T
+{
+    public Action<T> OnOptionalFilled;
+    public Action OnOptionalEmptied;
+    
+    public override void SetValue(K? value)
+    {
+        if (value == null)
+        {
+            Empty = true;
+            OnOptionalEmptied?.Invoke();
+        }
+        else
+        {
+            Empty = false;
+            Value = value;
+            OnOptionalFilled?.Invoke(Value);
+        }
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        int optionalsCount = Convert.ToInt32(Console.ReadLine());
+
+        Optional<int,int>[] optionals = new Optional<int,int>[optionalsCount];
+        
+        for (int i = 0; i < optionals.Length; i++)
+        {
+            Console.WriteLine($"Введите {i}-й елемент");
+            int value;
+            if (int.TryParse(Console.ReadLine(), out value))
+            {
+                optionals[i] = new Optional<int,int>(value);
+            }
+            else
+            {
+                optionals[i] = new Optional<int,int>(null);
+            }
+        }
+
+        foreach (var optional in optionals)
+        {
+            Console.Write(optional + " ");
+        }
+        
+        Console.Write("\n");
+        ExtendedOptional<int,int> extendedOptional = new ExtendedOptional<int,int>();
+        extendedOptional.OnOptionalEmptied += OnOptionalEmptiedHandler;
+        extendedOptional.OnOptionalFilled += OnOptionalFilledHandler;
+        extendedOptional.SetValue(42);
+        extendedOptional.SetValue(null);
+    }
+
+    public static void OnOptionalFilledHandler(int value)
+    {
+        Console.WriteLine($"Присвоено новое значение - {value}");
+    }
+
+    public static void OnOptionalEmptiedHandler()
+    {
+        Console.WriteLine("Значение опусташено");
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,103 @@
+﻿public static class Program
+{
+    public static void Main()
+    {
+       
+    }
+    
+    // Task 2
+    
+    public delegate void ValueDelegate<TValue>(TValue value);
+    
+    public static void ListMethodInvoke<TValue>(List<TValue> list, ValueDelegate<TValue> del)
+    {
+        foreach (var value in list)
+        {
+            del(value);
+        }
+    }
+    
+    // Task 3
+    
+    public static List<TResult> ListMethodInvoke<TValue, TResult>(List<TValue> list, Func<TValue, TResult> func)
+    {
+        List<TResult> results = new List<TResult>();
+        
+        foreach (var value in list)
+        {
+            results.Add(func(value));
+        }
+        
+        return results;
+    }
+    
+    // Task 4
+    
+    public static void FilterList<TValue>(List<TValue> list, Predicate<TValue> filter)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            TValue value = list[i];
+            if (!filter(value))
+            {
+                list.Remove(value);
+            }
+        }
+    }
+    
+    // Task 5
+    
+    public static List<TValue> MergeLists<TValue>(List<TValue> list1, List<TValue> list2, Func<List<TValue>,List<TValue>,List<TValue>> merge)
+    {
+        return merge(list1, list2);
+    }
+    
+    // Task 6
+
+    public static void SortList<TValue>(List<TValue> list, Comparison<TValue> compare)
+    {
+        SortList(list,compare,0,list.Count);
+    }
+
+    private static void SortList<TValue>(List<TValue> list,Comparison<TValue> compare,int leftIndex, int rightIndex)
+    {
+        int i = leftIndex;
+        int j = rightIndex;
+        var pivot = list[leftIndex];
+        
+        while (i <= j)
+        {
+            while (compare(list[i],pivot) < 0)
+            {
+                i++;
+            }
+        
+            while (compare(list[j],pivot) > 0)
+            {
+                j--;
+            }
+            
+            if (i <= j)
+            {
+                (list[i], list[j]) = (list[j], list[i]);
+                i++;
+                j--;
+            }
+        }
+    
+        if (leftIndex < j)
+            SortList(list,compare, leftIndex, j);
+        if (i < rightIndex)
+            SortList(list,compare, i, rightIndex);
+    }
+    
+    // Task 7
+    
+    public static void ListMethodInvoke<TValue>(TValue[] array, Action<TValue> action)
+    {
+        foreach (var value in array)
+        {
+            action(value);
+        }
+    }
+}
